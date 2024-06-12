b9c5c9dd6ddb6189cda54bad545ba5f4d58e2685
GodtoL <eliasvera793@gmail.com>
2024-06-12 11:06:38 -0400

    Delete dia2.py

--- dia2.py
+++
@@ -1,7 +0,0 @@
-def multiply_number(number):
-    for i in range(11):
-        multiply = number * i
-        print(f"{number} x {i} = {multiply}" )
-
-number = int(input("Ingrese el numero que desea su tabla: "))
-multiply_number(number)
